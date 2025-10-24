2670dab2db3781ff327f6d9b64bf877fc432f8d7
이형준 <hyeongjun@daily-funding.com>
2025-10-24 13:47:59 +0900

    ENH:메타태그 추가

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,38 @@
+import { type Metadata } from 'next';
+
 import { getNotionCategories } from '../../server/get-notion-categories';
 import { getNotionPages } from '../../server/get-notion-pages';
 import { NotionDomainPageClient } from './page-client';
+
+export const metadata: Metadata = {
+  title: 'HJ의 기술블로그',
+  description: 'HJ Tech Blog',
+  openGraph: {
+    title: 'HJ의 기술블로그',
+    description: 'HJ Tech Blog',
+    type: 'website',
+    url: 'https://hjlee7856.com',
+    images: [
+      {
+        url: 'https://hjlee7856.com/og-image.png',
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'HJ의 기술블로그',
+    description: 'HJ Tech Blog',
+    images: [
+      {
+        url: 'https://hjlee7856.com/og-image.png',
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
+};
 
 export default async function NotionDomainPage() {
   const { data, total } = await getNotionPages(false, 1, 10);
