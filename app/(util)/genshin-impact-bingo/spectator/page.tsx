0f90a32943adb297c6ad588b8d478ac816c3270c
이형준 <hyeongjun@daily-funding.com>
2025-11-26 13:57:36 +0900

    FIX: 메타데이터 수정

--- app/(util)/genshin-impact-bingo/spectator/page.tsx
+++ app/(util)/genshin-impact-bingo/spectator/page.tsx
@@ -3,8 +3,33 @@ import type { Metadata } from 'next';
 import { SpectatorPanel } from './components';
 
 export const metadata: Metadata = {
-  title: '원신 빙고 관전 모드',
-  description: '원신 캐릭터 빙고 게임을 실시간으로 관전하세요',
+  title: '원직쉼 송년회 빙고보드 관전 페이지',
+  description: '실시간으로 빙고 게임을 관전하세요!',
+  openGraph: {
+    title: '원직쉼 송년회 빙고보드 관전 페이지',
+    description: '실시간으로 빙고 게임을 관전하세요!',
+    type: 'website',
+    url: 'https://bingo-genshin-impact.vercel.app/genshin-impact-bingo/spectator',
+    images: [
+      {
+        url: 'https://bingo-genshin-impact.vercel.app/genshin-impact/og-img.jpg',
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: '원직쉼 송년회 빙고보드 관전 페이지',
+    description: '실시간으로 빙고 게임을 관전하세요!',
+    images: [
+      {
+        url: 'https://bingo-genshin-impact.vercel.app/genshin-impact/og-img.jpg',
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
 };
 
 export default async function SpectatorPage() {
